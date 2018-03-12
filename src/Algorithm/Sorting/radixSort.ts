14757cfe525bdfca1df23322834725f701ec6337
pilagod <pilagooood@gmail.com>
2018-03-12 21:22:13 +0800

    [feature] add Sorting/radixSort

--- src/Algorithm/Sorting/radixSort.ts
+++ src/Algorithm/Sorting/radixSort.ts
@@ -1,3 +1,45 @@
 export default function radixSort(nums: number[]): number[] {
-  return []
+  let strs = transformNumsToStrs(nums)
+  const maxDigits = strs.reduce((result, str) => Math.max(result, str.length), 0)
+  const radixes = createRadixes()
+
+  for (let i = 0; i < maxDigits; i++) {
+    strs.forEach((str) => {
+      if (str.length < i + 1) {
+        radixes['0'].push(str)
+      } else {
+        radixes[str[str.length - i - 1]].push(str)
+      }
+    })
+    strs.length = 0
+
+    for (const digit of Object.keys(radixes)) {
+      strs = strs.concat(radixes[digit])
+      radixes[digit] = []
+    }
+  }
+  return transformStrsToNums(strs)
+}
+
+function transformNumsToStrs(nums: number[]): string[] {
+  return nums.reduce((result, num) => {
+    result.push(num.toString())
+    return result
+  }, <string[]>[])
+}
+
+function createRadixes(): { [digit: string]: string[] } {
+  const result: { [digit: string]: string[] } = {}
+
+  for (let i = 0; i < 10; i++) {
+    result[i.toString()] = []
+  }
+  return result
+}
+
+function transformStrsToNums(strs: string[]): number[] {
+  return strs.reduce((result, str) => {
+    result.push(parseInt(str, 10))
+    return result
+  }, <number[]>[])
 }
--- test/Algorithm/Sorting/testCasesRunner.ts
+++ test/Algorithm/Sorting/testCasesRunner.ts
@@ -13,5 +13,37 @@ export default function testCasesRunner(
 
       expect(result).to.deep.equal([])
     })
+
+    it('should sort [1] to [1]', () => {
+      const nums = [1]
+
+      const result = sort(nums)
+
+      expect(result).to.deep.equal([1])
+    })
+
+    it('should sort [3, 2, 1] to [1, 2, 3]', () => {
+      const nums = [3, 2, 1]
+
+      const result = sort(nums)
+
+      expect(result).to.deep.equal([1, 2, 3])
+    })
+
+    it('should sort [23, 103, 15, 2] to [2, 15, 23, 103]', () => {
+      const nums = [23, 103, 15, 2]
+
+      const result = sort(nums)
+
+      expect(result).to.deep.equal([2, 15, 23, 103])
+    })
+
+    it('should sort [234, 446, 34, 23, 56, 1, 4787, 102, 28, 74, 91] to [1, 23, 28, 34, 56, 74, 91, 102, 234, 446, 4787]', () => {
+      const nums = [234, 446, 34, 23, 56, 1, 4787, 102, 28, 74, 91]
+
+      const result = sort(nums)
+
+      expect(result).to.deep.equal([1, 23, 28, 34, 56, 74, 91, 102, 234, 446, 4787])
+    })
   })
 }
